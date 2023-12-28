fbbf49d3ff220a2884bb170d585254f2b338eff2
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-28 19:42:05 +0545

    Initial

--- Task/urls.py
+++ Task/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',include('Auth.urls.py')),
+    path('',include('Auth.urls')),
+    path('',include('TaskManager.urls')),
 ]

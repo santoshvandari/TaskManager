a0823d76cb36c221d12a172af457e59c50be8d4c
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-29 19:13:07 +0545

    Updated

--- Auth/urls.py
+++ Auth/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path,include
 from Auth import views
 
 urlpatterns = [
+    path('',include("TaskManager.urls")),
     path('login/',views.log_in,name='login'),
     path('signup/',views.signup,name='signup'),
 ]
--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path,include
+from django.urls import path
+from TaskManager import views
 
 urlpatterns = [
-    path('',include('Auth.urls.py')),
+    path('',views.home,name='home'),
 ]

2249936bbc820dc426dca0e2bdb4cb6ecbc44a2d
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-29 18:17:27 +0545

    Updated

--- Auth/views.py
+++ Auth/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def log_in(request):
-    return render(request,'index.html')
+    return render(request,'login.html')
 
 def signup(request):
     return render(request,'signup.html')

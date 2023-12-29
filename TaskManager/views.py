c5a477ec6c8386c0b3c2469f183644d80a60174f
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-29 19:21:02 +0545

    Updated

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+# from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
+    if not request.user.is_authenticated:
+        return redirect('/login')
     return render(request,'index.html')

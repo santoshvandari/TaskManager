0c39b9c0d015206eb1dd0e401476e654d6f36659
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 15:21:40 +0545

    Initial

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Model To Store the Task Details 
+class TaskInfo(models.Model):
+    task_desc = models.TextField()
+    task_status = models.BooleanField(default=False)
+    task_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.task_name
+    class Meta:
+        db_table = 'TaskInfo'
+        verbose_name_plural = 'TaskInfo'
+        ordering = ['-task_created']

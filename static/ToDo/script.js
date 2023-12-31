c8beb6f32efd364a6f76d68fd2e8880ef7d776dc
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 11:14:24 +0545

    Initial

--- static/ToDo/script.js
+++ static/ToDo/script.js
@@ -1,10 +1,14 @@
-let formDiv=document.querySelector(".form-wrapper");
-document.getElementById("addtask").addEventListener("click", function () {
-    if(formDiv.classList.contains("show")){
-        formDiv.classList.remove("show");
-        console.log("removed");
-    }else{
-        formDiv.classList.add("show");
-        console.log("showed");
-    } 
-});
+// let formDiv=document.querySelector(".form-wrapper");
+// let btn=document.getElementById("addtask");
+// btn.addEventListener("click", function () {
+//     if(formDiv.classList.contains("show")){
+//         formDiv.classList.remove("show");
+//         console.log("removed");
+//         btn.textContent="Add Task";
+        
+//     }else{
+//         formDiv.classList.add("show");
+//         console.log("showed");
+//         btn.textContent="Hide Task";
+//     } 
+// });

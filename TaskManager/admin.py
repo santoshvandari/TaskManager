a172fbe12559a3bb608a3f70f1cbf7a034a56204
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 15:27:37 +0545

    Initial

--- TaskManager/admin.py
+++ TaskManager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from TaskManager.models import TaskInfo
 
 # Register your models here.
+class TaskInfoDispaly(admin.ModelAdmin):
+    list_display = ['user','task_desc','task_status','task_created']
+    list_filter = ['user','task_status']
+    search_fields = ['user','task_desc','task_status','task_created']
+
+# Register the TaskInfo Model
+admin.site.register(TaskInfo,TaskInfoDispaly)
--- TaskManager/models.py
+++ TaskManager/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # Model To Store the Task Details 
 class TaskInfo(models.Model):
+    # linking the task table with username of the user 
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     task_desc = models.TextField()
     task_status = models.BooleanField(default=False)
     task_created = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.task_name
     class Meta:
         db_table = 'TaskInfo'
         verbose_name_plural = 'TaskInfo'

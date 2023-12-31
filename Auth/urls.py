e4b5952a06156671c6df61cd3db98ecf80f1e424
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 15:54:44 +0545

    Initial

--- Auth/urls.py
+++ Auth/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('',include("TaskManager.urls")),
     path('login/',views.log_in,name='login'),
     path('signup/',views.signup,name='signup'),
+    path('logout/',views.log_out,name='logout'),
 ]

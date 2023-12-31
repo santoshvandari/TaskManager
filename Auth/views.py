58730ab56422722c855966ce6f8d654c02a913eb
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-31 15:55:46 +0545

    Initial

--- Auth/views.py
+++ Auth/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import logout
 
 # Create your views here.
 def log_in(request):
@@ -11,3 +12,7 @@ def signup(request):
     if request.user.is_authenticated:
         return redirect('/')
     return render(request,'signup.html')
+
+def log_out(request):
+    logout(request)
+    return redirect('/login')

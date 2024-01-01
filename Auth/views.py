19a0a2a53f6b64be41cd8ba97bd304dddcf2a5a0
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 07:11:14 +0545

    initial

--- Auth/views.py
+++ Auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout,login,authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def log_in(request):

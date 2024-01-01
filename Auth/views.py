b1201c327684df0ea02804cd388d356728b9d596
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-01 07:09:57 +0545

    initial

--- Auth/views.py
+++ Auth/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login,authenticate
 
 # Create your views here.
 def log_in(request):
     if request.user.is_authenticated:
         return redirect('/')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if authenticate(username=username,password=password) is not None:
+            login(request)
+            return redirect('/')
+        else:
+            return render(request,'login.html',{'error':'Invalid Credentials'})
     
     return render(request,'login.html')
 

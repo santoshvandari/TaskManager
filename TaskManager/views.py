db44264a5b813245ff64955aa5a5034004418f61
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-02 07:24:38 +0545

    Initial

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from TaskManager.models import TaskInfo
 # Create your views here.
 def home(request):
     if not request.user.is_authenticated:
@@ -10,4 +11,9 @@ def home(request):
 def AddTask(request):
     if not request.user.is_authenticated:
         return redirect('/login')
-    
+    if request.method == 'POST':
+        task_desc = request.POST.get('taskname')
+        user = request.user
+        status=False
+        TaskInfo.objects.create(user=user,task_desc=task_desc)
+        return redirect('/addtask')

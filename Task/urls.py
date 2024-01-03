63387c8c4457c917a890e0430372ed3db176e222
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-03 18:59:41 +0545

    Task Manager Completed

--- Task/urls.py
+++ Task/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',include('Auth.urls')),
-    # path('',include('TaskManager.urls')),
 ]
